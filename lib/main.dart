6d434757f547ee697583487a7fdfd64a0807b23b
Ethereal111 <99631608+Ethereal111@users.noreply.github.com>
2022-06-04 15:21:45 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'GYM MEMBERSHIP SYSTEM',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
       ),
       home: LoginRegister(),
     );
